7474a96f0db4f3c20503d800d7fbfd737623d4f7
ghjzzhg <ghjzzhg@163.com>
2017-12-18 13:21:37 +0800

    测试类

---
+++ src/main/java/test/cn/zx/ghjmaven/action/bc/TestActionTest.java
@@ -0,0 +1,65 @@
+package test.cn.zx.ghjmaven.action.bc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* TestAction Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ʮ���� 7, 2017</pre> 
+* @version 1.0 
+*/ 
+public class TestActionTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: Test() 
+* 
+*/ 
+@Test
+public void testTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDatasByClass() 
+* 
+*/ 
+@Test
+public void testGetDatasByClass() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDatasByCssQuery() 
+* 
+*/ 
+@Test
+public void testGetDatasByCssQuery() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: printf(List<LinkTypeData> datas) 
+* 
+*/ 
+@Test
+public void testPrintf() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
